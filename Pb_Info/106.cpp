bc6aff310b9596e494c4bd878f4c3f3f3f6409c4
peachlock <saragema.copil@gmail.com>
2026-06-27 21:14:56 +0300

    pb(106): add solution

---
+++ Pb_Info/106.cpp
@@ -0,0 +1,21 @@
+/******************************************************************************
+Calculaţi minimul celor 3 numere date.
+Programul citeşte de la tastatură 3 numere întregi separate prin spaţii.
+Programul va afişa pe ecran cel mai mic dintre numerele citite.
+*******************************************************************************/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a<=b && a<=c)
+        cout << a << endl;
+    else if (b<=a && b<=c)
+        cout << b << endl;
+    else
+        cout << c << endl;
+
+    return 0;
+}
