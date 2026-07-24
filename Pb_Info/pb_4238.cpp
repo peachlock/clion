1169ffb01b13f8858952d970a384916558c3857b
peachlock <saragema.copil@gmail.com>
2026-07-24 14:14:09 +0300

    pb_4238: solution 1

---
+++ Pb_Info/pb_4238.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+void citire(int a[1001], int&n) {
+    cin >> n;
+    for (int i=1; i<=n; i++) {
+        cin >> a[i];
+    }
+}
+
+int NrZero(int a[], int n) {
+    int cnt=0;
+    for (int i=1; i<=n; i++) {
+        if (a[i] % 2 == 0) {
+            if (a[i] == 0) {
+                cnt++;
+            }
+        }
+    }
+    return cnt;
+}
+
+int main () {
+    int a[101], n;
+    citire(a, n);
+    cout << "Numarul de zerouri: " << NrZero(a,n) << endl;
+}
