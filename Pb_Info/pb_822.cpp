6af225d1576a9f4d8ddebb4e4b357f8736f22fe4
peachlock <saragema.copil@gmail.com>
2026-08-07 13:43:49 +0300

    pb_822: rezolvata si pt cazul special

--- Pb_Info/pb_822.cpp
+++ Pb_Info/pb_822.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 
 int nr_cif_zero(int n) {
-    if (n == 0)
-       return 0;
-    else if (n % 10 == 0)
+    if (n < 10) {
+        if (n == 0)
+            return 1;
+        else
+            return 0;
+    }
+     if (n % 10 == 0)
         return 1 + nr_cif_zero(n/10);
-        else return nr_cif_zero(n/10);
+     else
+         return nr_cif_zero(n/10);
 }
 
 int main() {
     int n;
     cin >> n;
-    if (n==0)
-        cout << 1;
-    else cout << nr_cif_zero(n);
+    cout << nr_cif_zero(n);
 }
