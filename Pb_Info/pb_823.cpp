cf7e1df3c37d8e82c4e35e6c215e07f7e32f346b
peachlock <saragema.copil@gmail.com>
2026-08-07 13:38:58 +0300

    problema recursiva

---
+++ Pb_Info/pb_823.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int sumcif(int n) {
+    if (n==0)
+        return 0;
+    else return n % 10 + sumcif(n/10);
+}
+
+int main() {
+    int n;
+    cin >> n;
+    cout << sumcif(n) << endl; 
+}
