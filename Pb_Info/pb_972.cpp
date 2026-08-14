bd6aad4014af0f196a71800abd47e2be66474ff1
peachlock <saragema.copil@gmail.com>
2026-08-14 11:05:32 +0300

    pb_972: finished solution

--- Pb_Info/pb_972.cpp
+++ Pb_Info/pb_972.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 int main() {
     char s[256];
+    int nr=0;
     cin.getline(s, 256);
 
-    for (int i=1; i<s-2; i++) {
-        if ()
-    }
+    for (int i=0; i < strlen(s); i++)
+        if (i > 0 && i < strlen(s)-1)
+            if (strchr("aeiou", s[i]) != 0 && strchr("aeiou", s[i-1]) == 0 && s[i-1] != ' ' && strchr("aeiou", s[i+1]) == 0 && s[i+1] != ' ')
+                nr++;
+
+    cout << nr;
 
 }
