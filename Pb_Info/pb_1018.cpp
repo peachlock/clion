03c9ca03b1b6530f650f4c0f0d545ba7f5ffc4f5
peachlock <saragema.copil@gmail.com>
2026-08-21 11:17:33 +0300

    pb_1018: solution

---
+++ Pb_Info/pb_1018.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int impare(int st, int dr, int v[]) {
+    if (st == dr) {
+        if (v[st] % 2 != 0)
+            return 1;
+        else
+            return 0;
+    }
+    else {
+        int mij= (st+dr)/2;
+        return impare(st, mij, v) + impare(mij+1, dr, v);
+    }
+}
+
+int main() {
+    int n, v[1001];
+    cin >> n;
+    for (int i=1; i<=n; i++) {
+        cin >> v[i];
+    }
+
+    cout << impare(1, n, v);
+}
